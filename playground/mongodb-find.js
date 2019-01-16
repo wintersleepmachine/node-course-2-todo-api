e275b66237c33e9867830a431b40c88b567b5516
Jesse Bon Iver <you@example.com>
2019-01-15 22:59:01 -0800

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,40 @@
+// const MongoClient = require("mongodb").MongoClient
+const {MongoClient, ObjectID} = require("mongodb")
+
+
+
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err){
+       return console.log("Unable to connect to MongoDB server")
+    }
+    console.log("Connected to MongoDb server")
+
+    // db.collection("Todos").find({
+    //     _id: new ObjectID("5c3eaa91c932c23b249efaed")
+    // }).toArray().then((docs)=> {
+    //     console.log("todos")
+    //     console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //     console.log("Unable to fetch ours todos", err)
+    // })
+
+    // db.close()
+
+
+    // db.collection("Todos").find().count().then((count)=> {
+    //     console.log(`Todos count: ${count}`)
+    // }, (err) => {
+    //     console.log("Unable to fetch ours todos", err)
+    // })
+
+    db.collection("Users").find({
+        name: "jen"
+    }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2))
+    }, (err) => {
+        console.log(err)
+    })
+
+})
+
