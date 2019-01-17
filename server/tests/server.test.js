bec1fce1c289b360fcf47a04816d2371ac8840a7
Jesse Bon Iver <you@example.com>
2019-01-17 12:47:17 -0800

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require("supertest");
 const {app} = require("./../server")
 const {Todo} = require("./../models/todo")
 
+const todos = [{
+    text: "First test todo"
+}, {
+    text: "second test todo"
+}]
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => {
         done()
     })
 })
@@ -26,7 +34,7 @@ describe('POST /todos', () => {
                 return done(err)
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1)
                 expect(todos[0].text).toBe(text)
                 done()
@@ -34,7 +42,6 @@ describe('POST /todos', () => {
             })
         })
         
-        let text = {}
 
         it("Should not create todo with invalid body data", (done) => {
             request(app)
@@ -47,9 +54,21 @@ describe('POST /todos', () => {
                     }
 
                     Todo.find().then((todos) => {
-                        expect(todos.length).toBe(0)
+                        expect(todos.length).toBe(2)
                         done()
                     }).catch((e) => done(e))
                 })
         })
+})
+
+describe("GET /todos", () => {
+    it ("should get all todos", (done) => {
+        request(app)
+        .get("/todos")
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2)
+        })
+        .end(done)
+    })
 })
