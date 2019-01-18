ccc77046cfb88564210b121a7a773a9bb2f46b20
Jesse Bon Iver <you@example.com>
2019-01-17 22:39:29 -0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongoDB://localhost:27017/TodoApp")
+mongoose.connect(process.env.MONGODB_URI || "mongoDB://localhost:27017/TodoApp")
 
 module.exports= {mongoose}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {User} = require("./models/user.js")
 
 
 let app = express()
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
@@ -51,8 +52,9 @@ app.get("/todos/:id", (req,res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Started on port 3000")
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`)
 })
 
 
