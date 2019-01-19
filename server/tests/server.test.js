b84f4d57d29657146952646364cee7367439fbd1
Jesse Bon Iver <you@example.com>
2019-01-18 16:34:10 -0800

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -106,3 +106,46 @@ describe("GET /todos/:id", () => {
 
 
 })
+
+
+describe("DELETE /todos/:id", () => {
+    it("Should remove a todo", (done) => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId)
+            })
+            .end((err,res) => {
+                if(err){
+                    return done(err)
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e))
+            })
+    })
+    
+
+    it ("Should return 404 if todo not found", (done) => {
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .expect((res) => {
+            expect(res.status).toBe(404)
+        }).end(done)
+    })
+
+    it ("Should return 404 if object id is invalid", (done) => {
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString() + "123"}`)
+        .expect(404)
+        .expect((res) => {
+            expect(res.status).toBe(404)
+        }).end(done)
+    })
+})
