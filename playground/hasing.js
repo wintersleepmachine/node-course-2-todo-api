2d822b4e8848fcbdabd56b6ce90715062e109150
Jesse Bon Iver <you@example.com>
2019-01-21 14:08:58 -0800

    Hash user password before saving

--- playground/hasing.js
+++ playground/hasing.js
@@ -1,17 +1,41 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
-let data = {
-    id: 10
-}
+let password = "123abc!"
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) =>{
+//         console.log(hash)
+//     })
+// })
+
+// let hashedPassword = "$2a$10$cSSRm7MUlo6I58Kv6q6uN.Fcuo1i1LwvIL26/0BXm9n8/oZd2SUOC"
+
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//     console.log(res)
+// })
 
 
 
-let token = jwt.sign(data, "123abc") //server to client
-let decoded = jwt.verify(token, "123abc") //client to server
 
-console.log(token)
-console.log("decoded", decoded)
+
+
+
+
+
+
+// let data = {
+//     id: 10
+// }
+
+
+
+// let token = jwt.sign(data, "123abc") //server to client
+// let decoded = jwt.verify(token, "123abc") //client to server
+
+// console.log(token)
+// console.log("decoded", decoded)
 
 
 
